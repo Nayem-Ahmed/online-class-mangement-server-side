fbcfa5a14a1c8a8f57acf8d5033559fe8a6aa16c
unknown <freelancernayem.me@gmail.comm>
2023-11-26 21:10:53 +0600

    crud oparetion

--- index.js
+++ index.js
@@ -13,32 +13,56 @@ const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PAS
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const techBisyCollection = client.db("BisyDb").collection("techBisy");
+        const addClassCollection = client.db("BisyDb").collection("addclass");
+
+        // tech page 
+        app.post('/tech', async (req, res) => {
+            const techitem = req.body;
+            const result = await techBisyCollection.insertOne(techitem);
+            res.send(result)
+        })
+
+        // add class
+        app.post('/addclass', async (req, res) => {
+            const addclass = req.body;
+            const result = await addClassCollection.insertOne(addclass);
+            res.send(result)
+        })
+
+        app.get('/addclass' ,async(req, res) => {
+            const result = await addClassCollection.find().toArray()
+            res.send(result)
+          })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
