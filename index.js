9e70f5e13db22dd3c1bfec2d3cfbd6ac3cd9abc2
unknown <freelancernayem.me@gmail.comm>
2023-11-30 20:17:14 +0600

    crud oparetion apply

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRETE_KEY);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -34,10 +35,10 @@ async function run() {
             const result = await techBisyCollection.insertOne(techitem);
             res.send(result)
         })
-        app.get('/tech' ,async(req, res) => {
+        app.get('/tech', async (req, res) => {
             const result = await techBisyCollection.find().toArray()
             res.send(result)
-          })
+        })
 
 
         // add class
@@ -47,45 +48,45 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/addclass' ,async(req, res) => {
+        app.get('/addclass', async (req, res) => {
             const result = await addClassCollection.find().toArray()
             res.send(result)
-          })
-          app.delete('/addclass/:id',async (req, res) => {
+        })
+        app.delete('/addclass/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await addClassCollection.deleteOne(query)
             res.send(result)
-      
-          })
-          app.get('/addclass/:id', async (req, res) => {
+
+        })
+        app.get('/addclass/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const result = await addClassCollection.findOne(filter)
             res.send(result)
-      
-          })
 
-          app.patch('/addclass/:id',async(req,res)=>{
+        })
+
+        app.patch('/addclass/:id', async (req, res) => {
             const updateclass = req.body;
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
-                $set:{
-                    title: updateclass.title ,
-                    name:  updateclass.name  ,
-                    email: updateclass.email  ,
-                    price: updateclass.price  ,
-                    description:updateclass.description  ,
-                    image:updateclass.image
+                $set: {
+                    title: updateclass.title,
+                    name: updateclass.name,
+                    email: updateclass.email,
+                    price: updateclass.price,
+                    description: updateclass.description,
+                    image: updateclass.image
                 }
-                
+
             }
-            const result = await addClassCollection.updateOne(filter,updatedDoc);        
+            const result = await addClassCollection.updateOne(filter, updatedDoc);
             res.send(result)
 
 
-          })
+        })
 
         //   app.get('/addclass/:email',async(req,res) =>{
         //     const email= req.params.email;
@@ -100,40 +101,59 @@ async function run() {
         app.post('/users', async (req, res) => {
             const users = req.body;
             // email uniqe
-            const query ={email: users.email}
+            const query = { email: users.email }
             const existingUser = await usersCollection.findOne(query);
             if (existingUser) {
-                return res.send({message : 'user already existing',insertedId: null})
-                
+                return res.send({ message: 'user already existing', insertedId: null })
+
             }
             const result = await usersCollection.insertOne(users);
             res.send(result)
         })
 
-        app.get('/users' ,async(req, res) => {
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
-          })
+        })
 
-        app.get('/users/:email',async(req,res) =>{
-            const email= req.params.email;
-            const query = {email:email}
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
             const result = await usersCollection.findOne(query)
             res.send(result)
         })
 
-        app.patch('/users/admin/:id', async(req,res)=>{
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
-                $set:{
-                    role : "admin"
+                $set: {
+                    role: "admin"
                 }
-                
+
             }
-            const result = await usersCollection.updateOne(filter,updatedDoc);        
+            const result = await usersCollection.updateOne(filter, updatedDoc);
             res.send(result)
 
+        })
+
+
+        // payment
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const { price } = req.body;
+            const amount = parseInt(price * 100);
+
+            // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                payment_method_types: ["card"],
+
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+              });
         })
 
 
